2726c1467e39931d32e2cb09f3529898e414592f
albertying <albertlying@gmail.com>
2025-07-29 18:26:29 -0400

    refactor to run at the same time

--- device/device.py
+++ device/device.py
@@ -6,37 +6,38 @@ import device_pb2_grpc
 import asyncio
 import random
 
-async def thermometer():
-    # return a number from 60 to 90 with .1 increments e.g. 70.2, 60.9
-    asyncio.sleep(3)
-    return 60 + (random.randint(0, 300) * .1)
+async def thermometer_device(stream):
+    while True:
+        asyncio.sleep(3)
+        temperature =  60 + (random.randint(0, 300) * .1)
+        temperatureData = device_pb2.Data(device_id="thermo_001", timestamp = datetime.utcnow().isoformat() + "Z", device_type=device_pb2.DeviceType.THERMOMETER, temperature = temperature)
+        await stream.write(temperatureData)
 
-async def smart_plug():
-    asyncio.sleep(1)
-    return (random.randint(0, 15000) * .1)
+async def smart_plug_device(stream):
+    while True:
+        asyncio.sleep(1)
+        wattage = (random.randint(0, 15000) * .1)
+        wattageData = device_pb2.Data(device_id="smart_out_203", timestamp = datetime.utcnow().isoformat() + "Z", device_type=device_pb2.DeviceType.THERMOMETER, temperature = wattage)
+        await stream.write(wattageData)
 
-async def motion_sensor():
-    asyncio.sleep(5)
-    return random.choice([True, False])
-
+async def motion_sensor_device(stream):
+    while True:
+        asyncio.sleep(5)
+        motion = random.choice([True, False])
+        motionData = device_pb2.Data(device_id="motion_door_507", timestamp = datetime.utcnow().isoformat() + "Z", device_type=device_pb2.DeviceType.THERMOMETER, temperature = motion)
+        await stream.write(motionData)
+        
 async def simulate_devices():
     async with grpc.aio.insecure_channel('localhost:50051') as channel:
         stub = device_pb2_grpc.DeviceServiceStub(channel)
 
         stream = stub.StreamDeviceData()
 
-        while True:
-            temperature = await thermometer()
-            wattage = await smart_plug()
-            motion = await motion_sensor()
-
-            temperatureData = device_pb2.Data(device_id="thermo_001", timestamp = datetime.utcnow().isoformat() + "Z", device_type=thermometer, temperature = temperature)
-            wattageData = device_pb2.Data(device_id="smart_out_203", timestamp = datetime.utcnow().isoformat() + "Z", device_type=smart_plug, temperature = wattage)
-            motionData = device_pb2.Data(device_id="motion_door_507", timestamp = datetime.utcnow().isoformat() + "Z", device_type=motion_sensor, temperature = motion)
-
-            await stream.write(temperatureData)
-            await stream.write(wattageData)
-            await stream.write(motionData)
+        asyncio.gather(
+            thermometer_device(stream),
+            smart_plug_device(stream),
+            motion_sensor_device(stream),
+        )
 
 if __name__ == '__main__':
     asyncio.run(simulate_devices())
