1aa777c5e4c2cb5dcbead10b8fe98dacaf424512
albertying <albertlying@gmail.com>
2025-08-08 20:17:51 -0400

    use alert manager pattern

--- server/server.py
+++ server/server.py
@@ -46,32 +46,63 @@ class SmartPlugStrategy(DeviceStrategy):
             return True
         else:
             return False
+
+
+class AlertManager:
+    def __init__(self):
+        self.subscriptions = collections.defaultdict(set)
+        self.clients = {}
     
 class AlertService(alert_pb2_grpc.AlertServiceServicer):
+    def __init__(self, alert_manager: AlertManager):
+        self.alert_manager = alert_manager
     async def StreamAlerts(self, request_iterator, context):
-        mapping = collections.defaultdict(list)
         async for request in request_iterator:
             if request.HasField("subscribe"):
-                mapping[request.subscribe.client_id].append(request.subscribe.device_id)
+                client_id = request.subscribe.client_id
+                device_id = request.subscribe.device_id
+                self.alert_manager.subscriptions[client_id].add(device_id)
+                self.alert_manager.clients[client_id] = context
+
+                yield alert_pb2.AlertResponse(
+                    ack = alert_pb2.AckResponse(
+                        message = f"Subscribed to {device_id}",
+                        success = True
+                    )
+                )
             elif request.HasField("unsubscribe"):
-                mapping[request.unsubscribe.client_id].remove(request.unsubscribe.device_id)
+                client_id = request.subscribe.client_id
+                device_id = request.subscribe.device_id
+                self.alert_manager.subscriptions[client_id].discard(device_id)
+
+                yield alert_pb2.AlertResponse(
+                    ack = alert_pb2.AckResponse(
+                        message = f"Unsubscribed to {device_id}",
+                        success = True
+                    )
+                )
         
             
-            
-
-
 class DeviceService(device_pb2_grpc.DeviceServiceServicer):
+    def __init__(self, alert_manager: AlertManager):
+        self.alert_manager = alert_manager
+        self.device_strategies = {
+            "thermo": ThermometerStrategy(100),
+            "motion": MotionSensorStrategy(True),
+            "plug": SmartPlugStrategy(450)
+        }
+        
     async def StreamDeviceData(self, request_iterator, context):
-
-        thermometer_strategy = ThermometerStrategy(100)
-        motion_sensor_strategy = MotionSensorStrategy(True)
-        smart_plug_strategy = SmartPlugStrategy(450)
 
         async for data in request_iterator:
             payload_type = data.WhichOneof("payload")
             payload_value = getattr(data, payload_type)
 
             print(f"received device_id={data.device_id}, type={data.device_type}, timestamp={data.timestamp}, payload_type={payload_type}, payload_value={payload_value}")
+
+            strategy = self.device_strategies.get(data.device_id)
+            if strategy and strategy.should_send(payload_value):
+                await self.send_alert_to_subscribers(data.device_id, f"Alert! Value = {payload_value}", data.timestamp)
 
 
         return device_pb2.Response(status = "Success")
