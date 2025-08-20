69f13a05fbfe4e5cbca9fd2fc11d2261dfae2aa3
albertying <albertlying@gmail.com>
2025-08-20 11:39:56 -0400

    use queues instead of context

--- server/server.py
+++ server/server.py
@@ -52,36 +52,50 @@ class SmartPlugStrategy(DeviceStrategy):
 class AlertManager:
     def __init__(self):
         self.subscriptions = collections.defaultdict(set)
-        self.clients = {}
+        self.queues = {}
     
 class AlertService(alert_pb2_grpc.AlertServiceServicer):
     def __init__(self, alert_manager: AlertManager):
         self.alert_manager = alert_manager
+
     async def StreamAlerts(self, request_iterator, context):
-        async for request in request_iterator:
-            if request.HasField("subscribe"):
-                client_id = request.subscribe.client_id
-                device_id = request.subscribe.device_id
-                self.alert_manager.subscriptions[client_id].add(device_id)
-                self.alert_manager.clients[client_id] = context
+        queue = asyncio.Queue()
+        client_id = None
 
-                yield alert_pb2.AlertResponse(
-                    ack = alert_pb2.AckResponse(
-                        message = f"Subscribed to {device_id}",
-                        success = True
-                    )
-                )
-            elif request.HasField("unsubscribe"):
-                client_id = request.unsubscribe.client_id
-                device_id = request.unsubscribe.device_id
-                self.alert_manager.subscriptions[client_id].discard(device_id)
+        async def handle_requests():
+            nonlocal client_id
+            async for request in request_iterator:
+                if request.HasField("subscribe"):
+                    client_id = request.subscribe.client_id
+                    device_id = request.subscribe.device_id
+                    self.alert_manager.subscriptions[client_id].add(device_id)
+                    self.alert_manager.queues[client_id] = queue
 
-                yield alert_pb2.AlertResponse(
-                    ack = alert_pb2.AckResponse(
-                        message = f"Unsubscribed to {device_id}",
-                        success = True
-                    )
-                )
+                    await queue.put(alert_pb2.AlertResponse(
+                        ack=alert_pb2.AckResponse(
+                            message=f"Subscribed to {device_id}",
+                            success=True
+                        )
+                    ))
+
+                elif request.HasField("unsubscribe"):
+                    client_id = request.unsubscribe.client_id
+                    device_id = request.unsubscribe.device_id
+                    self.alert_manager.subscriptions[client_id].discard(device_id)
+
+                    await queue.put(alert_pb2.AlertResponse(
+                        ack=alert_pb2.AckResponse(
+                            message=f"Unsubscribed from {device_id}",
+                            success=True
+                        )
+                    ))
+
+        request_task = asyncio.create_task(handle_requests())
+
+        while True:
+            response = await queue.get()
+            yield response
+
         
             
 class DeviceService(device_pb2_grpc.DeviceServiceServicer):
@@ -94,12 +108,18 @@ class DeviceService(device_pb2_grpc.DeviceServiceServicer):
         }
 
     async def send_alert_to_subscribers(self, device_id, message, timestamp):
+        device_id_str = str(device_id)
         for client_id, subscribed_devices in self.alert_manager.subscriptions.items():
-            if device_id in subscribed_devices:
-                context = self.alert_manager.clients.get(client_id)
-                if context is not None:
-                    alert_response = alert_pb2.AlertResponse(alert = alert_pb2.AlertNotification(device_id = device_id, message = message, timestamp = timestamp))
-                    await context.write(alert_response)
+            if device_id_str in subscribed_devices:
+                queue = self.alert_manager.queues.get(client_id)
+                if queue:
+                    await queue.put(alert_pb2.AlertResponse(
+                        alert=alert_pb2.AlertNotification(
+                            device_id=device_id_str,
+                            message=message,
+                            timestamp=timestamp
+                        )
+                    ))
         
     async def StreamDeviceData(self, request_iterator, context):
 
